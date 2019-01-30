d9c7d0cedb9322d18015c84ad2d35ffa4f6746ea
Faireum-Eric <ericzhang@faireum.io>
2019-01-30 22:22:27 +0800

    Add SafeMath lib

--- contracts/FaireumToken.sol
+++ contracts/FaireumToken.sol
@@ -1,2 +1,42 @@
 pragma solidity ^0.4.22;
 
+library SafeMath {
+
+  /**
+  * @dev Multiplies overflow validation.
+  */
+  function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+    if (a == 0) {
+      return 0;
+    }
+    uint256 c = a * b;
+    assert(c / a == b);
+    return c;
+  }
+
+  /**
+  * @dev Divisions overflow validation.
+  */
+  function div(uint256 a, uint256 b) internal pure returns (uint256) {
+    uint256 c = a / b;
+    return c;
+  }
+
+  /**
+  * @dev Substracts overflow validation.
+  */
+  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+    assert(b <= a);
+    return a - b;
+  }
+
+  /**
+  * @dev Adds overflow validation.
+  */
+  function add(uint256 a, uint256 b) internal pure returns (uint256) {
+    uint256 c = a + b;
+    assert(c >= a);
+    return c;
+  }
+}
+
