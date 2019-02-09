972b14eabcef2a5970382955c2fc5dcef298cc9b
eric <ericzhang@faireum.io>
2019-02-09 18:46:55 +0800

    finish the first version of FaireumToken

--- contracts/FaireumToken.sol
+++ contracts/FaireumToken.sol
@@ -204,4 +204,43 @@ contract FaireumToken is Pausable,ERC20 {
     return true;
   }
 
+  /**
+   * @dev Approve the passed address to spend the specified amount of tokens on behalf of msg.sender.
+   *
+   * Beware that changing an allowance with this method brings the risk that someone may use both the old
+   * and the new allowance by unfortunate transaction ordering. One possible solution to mitigate this
+   * race condition is to first reduce the spender's allowance to 0 and set the desired value afterwards:
+   * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
+   * @param _spender The address which will spend the funds.
+   * @param _value The amount of tokens to be spent.
+   */
+  function approve(address _spender, uint256 _value) public returns (bool) {
+    allowed[msg.sender][_spender] = _value;
+    emit Approval(msg.sender, _spender, _value);
+    return true;
+  }
+
+  /**
+   * @dev Function to check the amount of tokens that an owner allowed to a spender.
+   * @param _owner address The address which owns the funds.
+   * @param _spender address The address which will spend the funds.
+   * @return A uint256 specifying the amount of tokens still available for the spender.
+   */
+  function allowance(address _owner, address _spender) public view returns (uint256) {
+    return allowed[_owner][_spender];
+  }
+
+  /**
+   * @dev Burns some tokens.
+   * @param _value The amount of token to be burned.
+   */
+  function burn(uint256 _value) whenNotPaused public {
+    require(_value > 0, "Amount invalid.");
+    require(_value <= balances[msg.sender], "Amount overflow.");
+
+    balances[msg.sender] = balances[msg.sender].sub(_value);
+    totalSupply = totalSupply.sub(_value);
+    emit Burn(msg.sender, _value);
+  }
+
 }
