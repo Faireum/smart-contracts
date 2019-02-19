82b3c6037ef9532fcad323f6a1f2d0737b2ea4b1
eric <ericzhang@faireum.io>
2019-02-19 10:19:29 +0800

    necessary dependencies

--- test/FaireumToken.test.js
+++ test/FaireumToken.test.js
@@ -4,3 +4,16 @@
  *
  * @copyright by Faireum Foundation Limited
  */
+
+const { BN, constants, expectEvent, shouldFail, time } = require('openzeppelin-test-helpers');
+const { ZERO_ADDRESS } = constants;
+const { promisify } = require('util');
+
+const FaireumToken = artifacts.require('FaireumToken');
+const decimal = '18';
+const decimalBN = new BN('10').pow(new BN(decimal));
+const initialSupply = new BN("1200000000").mul(decimalBN);
+let balanceHolderMarketing = new BN("120000000").mul(decimalBN);
+const lockStart = 1552262400;
+
+
