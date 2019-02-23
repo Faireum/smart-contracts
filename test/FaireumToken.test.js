af48fece6438829a66e29205ad7200f7a19e14e5
eric <ericzhang@faireum.io>
2019-02-23 17:00:59 +0800

    TokensVault test cases

--- test/FaireumToken.test.js
+++ test/FaireumToken.test.js
@@ -58,6 +58,103 @@ contract('FaireumToken', ([sender, receiver, accounts, anotherAccount, recipient
         });
     });
 
+    describe('onlyAdmin', function () {
+
+        it('allows the admin account to call onlyAdmin functions', async function () {
+            await this.token.createTokensVaults({ from: sender });
+        });
+
+        it('reverts when anyone calls onlyAdmin functions', async function () {
+            await shouldFail.reverting(this.token.createTokensVaults({ from: anotherAccount }));
+        });
+    })
+
+    describe('token vaults', function () {
+
+        describe('the TokensVaults before the createTokensVaults called', function () {
+
+            it('teamAdvisorsTokensVault should not be 0x', async function () {
+                (await this.token.teamAdvisorsTokensVault()).should.equal(ZERO_ADDRESS);
+            });
+
+            it('rewardPoolTokensVault should not be 0x', async function () {
+                (await this.token.rewardPoolTokensVault()).should.equal(ZERO_ADDRESS);
+            });
+
+            it('foundersTokensVault should not be 0x', async function () {
+                (await this.token.foundersTokensVault()).should.equal(ZERO_ADDRESS);
+            });
+
+            it('marketingAirdropTokensVault should not be 0x', async function () {
+                (await this.token.marketingAirdropTokensVault()).should.equal(ZERO_ADDRESS);
+            });
+
+            it('saleTokensVault should not be 0x', async function () {
+                (await this.token.saleTokensVault()).should.equal(ZERO_ADDRESS);
+            });
+        })
+
+
+        describe('the TokensVaults after the createTokensVaults function called', function () {
+
+            beforeEach(async function () {
+                await this.token.createTokensVaults();
+            });
+
+            it('teamAdvisorsTokensVault should be 0x', async function () {
+                (await this.token.teamAdvisorsTokensVault()).should.not.equal(ZERO_ADDRESS);
+            });
+
+            it('rewardPoolTokensVault should be 0x', async function () {
+                (await this.token.rewardPoolTokensVault()).should.not.equal(ZERO_ADDRESS);
+            });
+
+            it('foundersTokensVault should be 0x', async function () {
+                (await this.token.foundersTokensVault()).should.not.equal(ZERO_ADDRESS);
+            });
+
+            it('marketingAirdropTokensVault should be 0x', async function () {
+                (await this.token.marketingAirdropTokensVault()).should.not.equal(ZERO_ADDRESS);
+            });
+
+            it('saleTokensVault should be 0x', async function () {
+                (await this.token.saleTokensVault()).should.not.equal(ZERO_ADDRESS);
+            });
+        })
+
+        describe('the initial TokensVaults balance after the createTokensVaults function called', function () {
+
+            beforeEach(async function () {
+                await this.token.createTokensVaults();
+            });
+
+            it('createTokensVaults function cant be called twice', async function () {
+                await shouldFail.reverting(this.token.createTokensVaults());
+            });
+
+            it('teamAdvisorsTokensVault initial balance is correct', async function () {
+                (await this.token.balanceOf(await this.token.teamAdvisorsTokensVault())).should.be.bignumber.equal(new BN(120000000).mul(decimalBN));
+            });
+
+            it('rewardPoolTokensVault initial balance is correct', async function () {
+                (await this.token.balanceOf(await this.token.rewardPoolTokensVault())).should.be.bignumber.equal(new BN(240000000).mul(decimalBN));
+            });
+
+            it('foundersTokensVault initial balance is correct', async function () {
+                (await this.token.balanceOf(await this.token.foundersTokensVault())).should.be.bignumber.equal(new BN(60000000).mul(decimalBN));
+            });
+
+            it('marketingAirdropTokensVault initial balance is correct', async function () {
+                (await this.token.balanceOf(await this.token.marketingAirdropTokensVault())).should.be.bignumber.equal(new BN(120000000).mul(decimalBN));
+            });
+
+            it('saleTokensVault initial balance is correct', async function () {
+                (await this.token.balanceOf(await this.token.saleTokensVault())).should.be.bignumber.equal(new BN(660000000).mul(decimalBN));
+            });
+        })
+
+    });
+
 
 
 });
